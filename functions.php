175d62c0701b213ac4bdf92ae9cb491b2534dc19
lenisvillegas <lvillegas@alphaworks.tech>
2021-09-13 11:53:09 -0700

    added the logo function

--- functions.php
+++ functions.php
@@ -2,3 +2,18 @@
 
 // custom logo theme support
 add_theme_support( 'custom-logo' );
+
+//customizer support
+function basicwptheme_custom_logo_setup() {
+    $defaults = array(
+        'height'               => 100,
+        'width'                => 400,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+    );
+ 
+    add_theme_support( 'custom-logo', $defaults );
+}
+ 
+add_action( 'after_setup_theme', 'basicwptheme_custom_logo_setup' );
