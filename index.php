a1b3aa461c18abbc57c6e75781c261a1f53bf4ea
lenisvillegas <lvillegas@alphaworks.tech>
2021-09-13 11:31:29 -0700

    index template

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <div id="container">
+      <!-- header area -->
+      <header class="main-header">
+          <div class="custom-logo">
+            </div>
+            <div class="search">
+            </div>
+            <div class="main-nav">
+            </div>
+        </header>
+
+        <!-- banner/slider area -->
+     <section class="banner">
+        </section> 
+
+        <!-- main content area -->
+            <section class="content-holder">
+            <article class="content">
+            </article>    
+            <article class="sidebar">
+            </article>
+        </section>
+
+        <!-- footer area -->
+        <footer class="main footer">
+            <aside class="footer-box"></aside>
+            <aside class="footer-box"></aside>
+            <aside class="footer-box"></aside>
+            <aside class="footer-box"></aside>
+        </footer>
+        <div class="copywrite">
+        </div> 
+  </div>
+</body>
+</html>
