d5adc57a900df0c1f5ee2fabe8a74496758fd3a9
lenisvillegas <lvillegas@alphaworks.tech>
2021-09-14 14:38:24 -0700

    added logo , nav items , and banner in index and functions

--- functions.php
+++ functions.php
@@ -1,12 +1,14 @@
 <?php
+// theme support
 
-// custom logo theme support
+
+// custom logo customizer support
 add_theme_support( 'custom-logo' );
 
-//customizer support
+// featured image
+add_theme_support('post-thumbnails');
 
-//custom logo
-
+//custom logo customizer support-set height and width
 function basicwptheme_custom_logo_setup() {
     $defaults = array(
         'height'               => 100,
@@ -26,3 +28,27 @@ function theme_prefix_the_custom_logo() {
         the_custom_logo();
     }
 }
+
+
+// add HTML theme support for search bar
+function wpdocs_after_setup_theme() {
+    add_theme_support( 'html5', array( 'search-form' ) );
+}
+add_action( 'after_setup_theme', 'wpdocs_after_setup_theme' );
+
+
+// Registering side bar and widgets
+function custom_widgets() {
+
+	register_sidebar( array(
+		'name'          => 'Home Banner Slider',
+		'id'            => 'home_banner_slider',
+		'before_widget' => '',
+		'after_widget'  => '',
+		'before_title'  => '',
+		'after_title'   => '',
+	) );
+
+}
+                // hook...   name 
+add_action( 'widgets_init', 'custom_widgets' );
--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php wp_head(); ?>
 </head>
 <body>
+<?php wp_body_open( ); ?>
   <div id="container">
       <!-- header area -->
       <header class="main-header">
@@ -22,13 +24,27 @@
             ?>
             </div>
             <div class="search">
+            <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+                     <input type="search" class="search-field"
+                         placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+                         value="<?php echo get_search_query() ?>" name="s"
+                         title="<?php echo esc_attr_x( 'Search for...', 'label' ) ?>" />
+                     <input type="submit" class="search-submit"
+                         value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+            </form>
             </div>
             <div class="main-nav">
+                <?php wp_nav_menu(); ?>
+        
             </div>
         </header>
 
         <!-- banner/slider area -->
      <section class="banner">
+                  <?php if ( is_active_sidebar( 'custom_widgets' ) ) : ?>
+                  	<?php dynamic_sidebar( 'custom_widgets' ); ?>
+                        <?php endif; ?>
+
         </section> 
 
         <!-- main content area -->
